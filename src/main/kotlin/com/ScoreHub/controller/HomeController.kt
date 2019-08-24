761031e059d38a09d97c24d6481d5bb9a970f3c2
Ryohei <ryohei.mmeve@gmail.com>
2019-08-25 07:55:02 +0900

    initial commit

--- src/main/kotlin/com/ScoreHub/controller/HomeController.kt
+++ src/main/kotlin/com/ScoreHub/controller/HomeController.kt
@@ -1,10 +1,10 @@
-package ScoreHub
+package com.ScoreHub.controller
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import ScoreHub.repogitory.RedisRepository
+import com.ScoreHub.repogitory.RedisRepository
 
 @RestController
 @RequestMapping("home")
--- src/main/kotlin/com/ScoreHub/repogitory/RedisProperties.kt
+++ src/main/kotlin/com/ScoreHub/repogitory/RedisProperties.kt
@@ -1,4 +1,4 @@
-package ScoreHub.repogitory
+package com.ScoreHub.repogitory
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 
